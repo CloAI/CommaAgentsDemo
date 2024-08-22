e804c165022d1fffde0eb561f07430075a850dbb
Your Name <you@example.com>
2024-08-22 09:08:50 -0500

    Updated the running of the model.

--- reorganize_financials.py
+++ reorganize_financials.py
@@ -2,6 +2,7 @@ import random
 from comma_agents.hub.agents.cloai.mlx import MLXAgent
 from comma_agents.prompts import Llama31PromptTemplate
 from comma_agents.flows import SequentialFlow
+from comma_agents.code_interpreters import CodeInterpreter
 from strategies import DirectoryAndFileSummaryStrategy
 SEED = 4497
 # Set the seed of the program to keep the results reproducible and consistent
@@ -32,19 +33,51 @@ develop_balance_sheet_agent = MLXAgent(
             "system_message": """
 Environment: ipython
 
-You are a agent capable of generating a balance sheet for a company.
-You will be given a list of financial files and their summaries.
-You will will determine what files are needed for the balance sheet.
-You can use Python to help you generate the balance sheet and read the files.
+# Operation Instructions
+0. Determine the files needed for completing the Balance Sheet Requirements.
+1. Use Python to determine the files structure and data availability. Print headers, confirmation of data types, and account for errors.
+2. Once you have the necessary files load them and extract needed information.
+3. Generate a balance sheet for the company.
+4. If data needed is not available set the value to -1.
+
+# Balance Sheet Requirements
+- Net revenue
+- Cost of sales
+- Amortization of acquisition-related intangibles
+- Total cost of sales
+- Gross profit
+- Research and development
+- Marketing, general and administrative
+- Amortization of acquisition-related intangibles
+- Licensing gain
+- Operating income (loss) 
+- Interest expense
+- Other income (expense)
+- Income (loss) before income taxes and equity income
+- Income tax provision (benefit)
+- Equity income in investee
+- Net income (loss)
+
+# Errors
+- If there is an error with scripts notify and come up with a solution.
 """
         }
     ),
     config={
         "model_path": "mlx-community/Meta-Llama-3.1-8B-Instruct-8bit",
         "seed": SEED,
-        "max_tokens": 2048,
-        "temp": 0.0
-    }
+        "max_tokens": 2048 * 2,
+        "temp": 0.0,
+        "stream": True
+    },
+    interpret_code=True,
+    code_interpreter=CodeInterpreter(
+        execution_directory="./data/base_case_financials/Q1",
+        output_format="""<|start_header_id|>ipython<|end_header_id|>
+        
+{code_output}<|eot_id|>
+"""
+    )
 )
 
 balance_sheet_generator_flow = SequentialFlow(
